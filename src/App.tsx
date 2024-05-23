811ecc9e9d8c74bd2a91ef9c005fda5dbc5d65d7
Ederaldo Souza <84224107+EderSouza93@users.noreply.github.com>
2024-05-23 13:57:10 -0300

    Interface Criada

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,10 @@
+import { FiTrash } from 'react-icons/fi'
 
 export default function App(){
   return (
     <div className="w-full min-h-screen bg-gray-900 flex justify-center px-4">
-      <main className="my-10 w-full md:max-w-2x1">
-        <h1 className="text-4x1 font-medium text-white">Clientes</h1>
+      <main className="my-10 w-full md:max-w-2xl">
+        <h1 className="text-4xl font-medium text-white">Clientes</h1>
 
         <form className="flex flex-col my-6">
           <label className="font-medium text-white">Nome:</label>
@@ -28,10 +29,14 @@ export default function App(){
         </form>
 
         <section className="flex flex-col">
-          <article className="w-full bg-white rounded p-2">
+          <article className="w-full bg-white rounded p-2 relative hover:scale-105 duration:-200">
             <p><span className="font-medium">Nome:</span> Eder</p>
             <p><span className="font-medium">Email:</span> eder@teste.com</p>
             <p><span className="font-medium">Status:</span> Ativo</p>
+
+            <button className='bg-red-500 w-7 h-7 flex items-center justify-center rounded-lg absolute right-0 -top-2'>
+              <FiTrash size={18} color="#FFF"/>
+            </button>
           </article>
         </section>
       </main>
